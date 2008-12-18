b5f788ca56a8b84c68bb64ab67e12d91cf2c8e5c
diegoiast <diegoiast@29d931e5-ce36-0410-8c75-bbf2102e2ead>
2008-12-18 22:16:38 +0000

    more indentation fixes

--- tools/project-manager/pm-gui/mainwindow.h
+++ tools/project-manager/pm-gui/mainwindow.h
@@ -9,23 +9,22 @@ class QAbstractItemModel;
 
 class MainWindow : public QMainWindow
 {
-    Q_OBJECT
-
+	Q_OBJECT
+	
 public:
-    MainWindow(QWidget *parent = 0, Qt::WFlags flags = 0);
-    ~MainWindow();
-
+	MainWindow(QWidget *parent = 0, Qt::WFlags flags = 0);
+	~MainWindow();
+	
 private slots:
-    void on_quitButton_clicked();
-    void on_deleteButton_clicked();
-    void on_addButton_clicked();
-    void on_loadButton_clicked();
-
+	void on_quitButton_clicked();
+	void on_deleteButton_clicked();
+	void on_addButton_clicked();
+	void on_loadButton_clicked();
+	
 private:
-    Ui::MainWindowClass ui;
-    AbstractProject *m_project;
-    QAbstractItemModel *m_model;
-
+	Ui::MainWindowClass ui;
+	AbstractProject *m_project;
+	QAbstractItemModel *m_model;
 };
 
 #endif // MAINWINDOW_H
