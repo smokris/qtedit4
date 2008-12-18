2209af2789b63c9974a474ac51666a97d7cceed6
diegoiast <diegoiast@29d931e5-ce36-0410-8c75-bbf2102e2ead>
2008-12-18 22:14:20 +0000

    Indentation

--- tools/project-manager/pm-gui/mainwindow.cpp
+++ tools/project-manager/pm-gui/mainwindow.cpp
@@ -6,21 +6,21 @@
 #include "projectmodel.h"
 
 MainWindow::MainWindow(QWidget *parent, Qt::WFlags flags)
-    : QMainWindow(parent, flags)
+	: QMainWindow(parent, flags)
 {
-    ui.setupUi(this);
-    m_project = NULL;
-    m_model = NULL;
+	ui.setupUi(this);
+	m_project = NULL;
+	m_model = NULL;
 }
 
 MainWindow::~MainWindow()
 {
-
+	
 }
 
 void MainWindow::on_quitButton_clicked()
 {
-    this->close();
+	this->close();
 }
 
 void MainWindow::on_deleteButton_clicked()
@@ -34,20 +34,20 @@ void MainWindow::on_addButton_clicked()
 void MainWindow::on_loadButton_clicked()
 {
 	qDebug() << __FILE__ << __LINE__;
-    QString fileName = QFileDialog::getOpenFileName(this, tr("Open project"),
-	"../tests/", tr("Projects (*.pro *.pri *.prf)"));
-    if (fileName.isEmpty())
-        return;
-
-    if (!m_project)
-        m_project = new QMakeProject( this );
-
-    m_project->loadProject( fileName );
-    ui.projectLabel->setText( fileName );
-
-    if (!m_model)
-    {
-        m_model = new ProjectModel( this, m_project );
-    }
-    ui.treeView->setModel( m_model );
+	QString fileName = QFileDialog::getOpenFileName(this, tr("Open project"),
+							"../tests/", tr("Projects (*.pro *.pri *.prf)"));
+	if (fileName.isEmpty())
+		return;
+	
+	if (!m_project)
+		m_project = new QMakeProject( this );
+	
+	m_project->loadProject( fileName );
+	ui.projectLabel->setText( fileName );
+	
+	if (!m_model)
+	{
+		m_model = new ProjectModel( this, m_project );
+	}
+	ui.treeView->setModel( m_model );
 }
